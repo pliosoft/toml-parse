0a0a66a55d0dd62672d3448884d83bc519104635
patrick brisbin <pbrisbin@gmail.com>
2016-04-11 11:38:21 -0400

    Implement readToml

--- src/Text/Toml.hs
+++ src/Text/Toml.hs
@@ -1,25 +1,12 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Text.Toml
     ( readToml
     ) where
 
-import Data.Text(Text)
-import qualified Data.Text as T
-import Data.Time.Clock(UTCTime)
-import Data.Int(Int64)
-import qualified Data.HashMap.Strict as Map
-import Data.Foldable
-import Data.String(IsString(..))
-import qualified Control.Lens as L
-import Prelude hiding (lookup)
+import Text.Toml.Parser
 import Text.Toml.Types.Toml
 
+import qualified Data.Text.IO as T
 
 -- | Given a toml document, will read it completely, producing a Toml document as a response
-readToml :: FilePath -> IO (Either Text Toml)
-readToml a = do putStrLn "readToml"
-                return $ Right (insert "four" (TInteger 4) empty)
-
-
+readToml :: FilePath -> IO (Either String Toml)
+readToml a = parseToml a <$> T.readFile a
